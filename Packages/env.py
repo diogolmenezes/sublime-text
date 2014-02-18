7aca06e8bd12cd50a51870d75b59e208a8d4ce5d
Nando Vieira <fnando.vieira@gmail.com>
2014-02-18 08:57:26 -0300

    Updates.

--- Packages/env.py
+++ Packages/env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["PATH"] = "/Users/fnando/local/node/npm/bin:/Users/fnando/local/node/current/bin:/Users/fnando/local/bin:/Users/fnando/local/ruby/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+os.environ["PATH"] = "/Users/fnando/local/bin:/Users/fnando/local/ruby/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 os.environ["GEM_HOME"] = "/Users/fnando/local/ruby/gems"
 os.environ["GEM_PATH"] = "/Users/fnando/local/ruby/gems"
 print ("PATH=%s" % os.environ["PATH"])
